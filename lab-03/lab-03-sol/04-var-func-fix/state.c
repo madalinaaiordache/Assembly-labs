63cc057f66c804410a6df2bcd489ef693f392e69
madalinaaiordache <74785299+madalinaaiordache@users.noreply.github.com>
2022-05-13 17:17:42 +0300

    Add files via upload

---
+++ lab-03/lab-03-sol/04-var-func-fix/state.c
@@ -0,0 +1,8 @@
+const char *shopping_list[3];
+
+void init_shopping(void)
+{
+	shopping_list[0] = "cheese";
+	shopping_list[1] = "wine";
+	shopping_list[2] = "dessert";
+}
---
+++ lab-03/lab-03-sol/bonus-c-python/python_modules/my_module.py
@@ -0,0 +1,5 @@
+def say(something='Something default'):
+    print(something)
+
+def contains(haystack='/', needle='\\'):
+    return haystack.find(needle)
